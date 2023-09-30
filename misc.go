b86b2cf94a97e83247a491497d9bd608d24e6f58
ClashKid155 <beluchukwunjike@gmail.com>
2023-09-30 12:56:21 +0100

    Add function to get anime name and episode from given string

--- misc.go
+++ misc.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strings"
 )
 
 // IsExists Check if a file exists
@@ -11,4 +12,14 @@ func IsExists(file string) bool {
 		return false
 	}
 	return err == nil
+}
+
+func GetNameAndEpisode(data string) (string, string) {
+	// Watch Dark Gathering - 12 Online
+	strip := strings.TrimSuffix(strings.TrimPrefix(data, "Watch "), " Online")
+	split := strings.Split(strip, "-")
+	name := strings.TrimSpace(split[0])
+	episode := strings.TrimSpace(split[1])
+
+	return name, episode
 }
